73c6215870f7e1aaa6b0cbae95a69c19b2c5e2a9
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2022-09-24 17:17:30 +0700

    week03, file01 completed

---
+++ MAT106004/week03/file01/exercise02.R
@@ -0,0 +1,28 @@
+# Exercise 02
+
+# data
+x = c(31, 52, 37, 21, 24,19, 23, 43, 21)
+
+# 1)
+sum(x)
+
+# 2)
+sum(x^2)
+
+# 3)
+sum(x) / length(x)
+
+# 4)
+length(x)
+
+# 5)
+sort(x)
+
+# 6)
+sort(x, decreasing = TRUE)
+
+# 7)
+max(x)
+
+# 8)
+min(x)
